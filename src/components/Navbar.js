76608dc340b5c0045ced5ee8edae0e37143e3c83
Animesh Dhara <animeshdhara398@gmail.com>
2023-09-24 22:47:25 +0530

    Fix the link error problem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className={`navbar navbar-expand-lg  bg-${props.modeType === 'dark'?'dark':'light'} navbar-${props.modeType === 'dark'?'dark':'light'}`}>
     <div className="container-fluid">
-      {/* <Link className="navbar-brand" href="/">Navbar</Link> */}
-      <a className="navbar-brand" href="#">{props.title}</a>
+      {/* <Link className="navbar-brand" to="/">Navbar</Link> */}
+      <a className="navbar-brand" href="/">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -15,7 +15,7 @@ export default function Navbar(props) {
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
             {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           {/* <li className="nav-item">
             <Link className="nav-link active" aria-current="page" to="/about">About</Link>
